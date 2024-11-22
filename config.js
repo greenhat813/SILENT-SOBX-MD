83acc365cae8404da16e6e94f9a6187b9c2c6e92
greenhat813 <greenhat813@gmail.com>
2024-11-22 13:35:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+nconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "zqJwDCzL#ajdPM8_02_E1EL4csC4kKT-53lfhhldhlqiy0_faflI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
